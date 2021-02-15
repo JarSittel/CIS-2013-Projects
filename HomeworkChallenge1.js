e21caafb80b7b280edead60bef4c78286c508456
Jarvis Sittel <34584517+JarSittel@users.noreply.github.com>
2021-02-15 10:52:40 -0600

    Add files via upload

---
+++ HomeworkChallenge1.js
@@ -0,0 +1,5 @@
+var floatMiles, floatGallons, floatMPG;
+floatMiles = parseFloat(prompt("Enter Miles Driven: "));
+floatGallons = parseFloat(prompt("Enter Gallons Used: "));
+floatMPG = floatMiles/floatGallons;
+alert("Miles per gallon = " + floatMPG);
