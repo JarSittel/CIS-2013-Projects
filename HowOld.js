0e7eaffa35da4272682bc6c07fe0425fb0959caf
Jarvis Sittel <34584517+JarSittel@users.noreply.github.com>
2021-02-15 11:12:45 -0600

    Delete HowOld.js

--- HowOld.js
+++
@@ -1,7 +0,0 @@
-var floatYears, floatDays, floatMonths, floatFortnights, floatWeeks;
-floatYears = parseFloat(prompt("How old are you?: "));
-floatDays = floatYears*365.25;
-floatFortnights = floatDays/14;
-floatWeeks = floatDays/7;
-floatMonths = floatYears * 12;
-alert("You've been alive for " + floatYears + " years, " + floatMonths + " months, " + floatFortnights + " fortnights, " + floatWeeks + " weeks, " + floatDays + " days.");
