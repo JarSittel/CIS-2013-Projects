457138871d90ff8aaaea28cebad8ef6047c3aeae
Jarvis Sittel <34584517+JarSittel@users.noreply.github.com>
2021-03-01 12:23:55 -0600

    Add files via upload

---
+++ Loan.js
@@ -0,0 +1,22 @@
+
+var $ = function (id) 
+{
+    return document.getElementById(id);
+}
+
+ var calculate = function()
+ {
+    var floatTotalCost, floatMonthlyPayment, intLoanTerm;
+    floatMonthlyPayment = $("monthly_payment").value;
+    intLoanTerm = $("loan_length").value;
+    floatTotalCost = floatMonthlyPayment*intLoanTerm;
+    $("total_cost").value = floatTotalCost;
+ }
+ 
+ window.onload = function () 
+{
+    $("loan_length").value = "";
+    $("total_cost").value = "";
+    $("calc").onclick = calculate();
+    $("monthly_payment").focus();
+}
